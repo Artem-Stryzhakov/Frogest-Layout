26474f2167dc347fbc8699aa35a292284f078879
Artem-Stryzhakov <Phernander1@gmail.com>
2023-01-28 00:30:44 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const nav_categories = document.querySelectorAll('.nav-categories ul li')
-console.log(nav_categories)
 
 nav_categories.forEach(buttons => buttons.addEventListener('click', () => {
     buttons.classList.toggle('focus-color')
