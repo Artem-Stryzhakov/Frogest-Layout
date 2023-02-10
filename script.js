619ccec83b25c0d2e648935abccf407e58cfb1d4
Artem-Stryzhakov <Phernander1@gmail.com>
2023-02-10 19:54:33 +0200

    Info page is finished.

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 const nav_categories = document.querySelectorAll('.nav-categories ul li')
+const collapseContainer = document.querySelectorAll('.collapse .card .collapse-container')
+const underCategories = document.querySelectorAll('.under-categories')
+const categoriesType = document.querySelectorAll('.under-categories p')
+const titleLinks = document.querySelectorAll('.under-categories h4 a')
+
+//Get the button
+const mybutton = document.getElementById("btn-back-to-top");
 
 nav_categories.forEach(buttons => buttons.addEventListener('click', () => {
     buttons.classList.toggle('focus-color')
 }))
+
+collapseContainer.forEach(collapseContainer => {
+    collapseContainer.style.fontSize = '10px'
+    collapseContainer.style.gap = '40px'
+})
+
+categoriesType.forEach(type => {
+    type.style.margin = '0px'
+    type.style.padding = '0px'
+})
+
+underCategories.forEach(category => category.style.marginTop = '-5px')
+
+titleLinks.forEach(titleLink => {
+    titleLink.style.fontSize = '20px'
+    titleLink.style.maxWidth = '200px'
+    titleLink.style.fontWeight = 'bold'
+})
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) ?
+        mybutton.style.display = "block" : mybutton.style.display = "none"
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
