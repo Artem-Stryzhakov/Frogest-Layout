b00095b711f0aef5e6de9728e7f2a7ce870337d2
Artem-Stryzhakov <Phernander1@gmail.com>
2023-02-14 19:05:30 +0200

    Working on it.

--- script.js
+++ script.js
@@ -31,18 +31,13 @@ titleLinks.forEach(titleLink => {
 
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
+window.onscroll = () => {
     (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) ?
         mybutton.style.display = "block" : mybutton.style.display = "none"
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+};
 
-function backToTop() {
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+});
